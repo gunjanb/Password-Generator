75902c2e05d93aaaf1fa89a0809e7c6f5cd7e7fb
Gunjan Bharagava <gunjanb2006@gmail.com>
2021-04-07 01:57:48 -0700

    listed all criteria for password generation in words

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,19 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+//Generate Password function will generate password
+//1. Ask user the lenght of password: has to be b/w 8 -128 chars.
+//2. Validate input lenght: check in b/w 8 -128 as well as it should be a number.
+//3. Ask user whether want to have a number ,special char,uppercase or lowercase in password.
+//4. Check whether user atleast selected one choice.
+//5. Generate password.
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
 
 // Add event listener to generate button
